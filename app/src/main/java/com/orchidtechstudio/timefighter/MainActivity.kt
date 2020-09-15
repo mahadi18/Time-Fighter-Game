63412b30bd9e7c18e4c7d6af0f6d504eda9913db
mahadi18 <mahadimohammad18@gmail.com>
2020-09-15 14:30:49 +0600

    Initial commit

---
+++ app/src/main/java/com/orchidtechstudio/timefighter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.orchidtechstudio.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
